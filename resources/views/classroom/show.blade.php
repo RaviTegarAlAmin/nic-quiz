a8df9bf138c62f900834d37d44d9fa4d61576def
Reidgear <tegaralamin@gmail.com>
2025-07-16 04:15:08 +0700

    Classroom Students Refactor

--- resources/views/classroom/show.blade.php
+++ resources/views/classroom/show.blade.php
@@ -4,21 +4,21 @@
     <x-header>Data Kelas</x-header>
 
     <div class="flex justify-center items-center">
-        <table class=" table-auto">
-            <thead class=" table-header-group">
-                <tr>
-                    <th>Nama</th>
+        <table class=" table-auto overflow-auto mb-10">
+            <thead>
+                <tr class="border border-slate-300 rounded-tr-md rounded-rl-md bg-slate-200">
+                    <th class="py-2 px-2">Nama</th>
                     <th>Jenis Kelamin</th>
                     <th>Alamat</th>
                     <th>Tanggal Lahir</th>
                 </tr>
             </thead>
             @foreach ($classroom->students as $student)
-                <tr class="even:bg-slate-200">
-                    <td class="py-2">{{ $student->name }}</td>
-                    <td>{{ $student->gender }}</td>
-                    <td>{{ $student->address }}</td>
-                    <td>{{ $student->born_date }}</td>
+                <tr class="even:bg-slate-200 border-b-2 border-black">
+                    <td class="py-2 px-3">{{ $student->name }}</td>
+                    <td class="px-3">{{ $student->gender }}</td>
+                    <td class="px-3">{{ $student->address }}</td>
+                    <td class="px-3">{{ $student->born_date }}</td>
                 </tr>
             @endforeach
         </table>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
-<div class=" my-2 px-2 py-6 mb-6 border border-slate-300 shadow-md bg-secondary-300 text-white">
+<div class=" my-2 px-5 py-6 mb-6 border border-slate-300 shadow-md bg-secondary-300 text-white text-xl font-medium rounded-xl">
     {{ $slot }}
 </div>
