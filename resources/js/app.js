4c9e442004d8c67d8c43deacd58cc3d1607d43f1
Reidgear <tegaralamin@gmail.com>
2026-06-03 12:41:34 +0700

    chore: install chart.js and expose chart globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import './exam-attempt';
 import './question-editor';
+import Chart from 'chart.js/auto';
 
 import { createIcons, icons } from 'lucide';
 import Quill from 'quill';
@@ -8,6 +9,7 @@ import katex from 'katex';
 
 window.Quill = Quill;
 window.katex = katex;
+window.Chart = Chart;
 
 document.addEventListener('DOMContentLoaded', () => {
     createIcons({ icons });
